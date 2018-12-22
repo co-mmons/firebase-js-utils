396f79927f0d0d4ca0028a156d8dc3841e30eaa1
Levy <levy@codeandmore.pl>
2018-12-22 17:08:45 +0100

    Commit

--- dist/esm/firestore/rxjs/index.js
+++ dist/esm/firestore/rxjs/index.js
@@ -1,10 +1,15 @@
+import { UniversalFirestore } from "../";
+import "./collection-query-snapshot-observable";
 import { collectionQuerySnapshotObservableInject } from "./collection-query-snapshot-observable";
+import "./doc-data-observable";
 import { docDataObservableInject } from "./doc-data-observable";
+import "./doc-snapshot-observable";
 import { docSnapshotObservableInject } from "./doc-snapshot-observable";
+import "./docs-data-observable";
 import { docsDataObservableInject } from "./docs-data-observable";
+import "./docs-snapshots-observable";
 import { docsSnapshotsObservableInject } from "./docs-snapshots-observable";
 export * from "../";
-import { UniversalFirestore } from "../";
 export function injectUniversalFirestoreRxjs() {
     UniversalFirestore;
     collectionQuerySnapshotObservableInject();
