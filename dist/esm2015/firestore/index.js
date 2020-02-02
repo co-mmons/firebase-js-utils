0b3f7d49cba25fcc3816308af9e2157b741b4b86
Levy <levy@codeandmore.pl>
2020-02-02 22:12:52 +0100

    Commit

--- dist/esm2015/firestore/index.js
+++ dist/esm2015/firestore/index.js
@@ -1,10 +1,11 @@
+export * from "./apply-converter";
 export * from "./auto-write-batch";
 export * from "./data-converter";
 export * from "./delete-query";
 export * from "./doc-data";
 export * from "./docs-data";
 export * from "./docs-snapshots";
+export * from "./mode";
 export * from "./types";
 export * from "./union-types";
-export * from "./mode";
 //# sourceMappingURL=index.js.map
