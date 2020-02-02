853cb78dba8a5ac2b54a82df15e0b91d9747374f
Levy <levy@codeandmore.pl>
2020-02-02 17:16:32 +0100

    Commit

--- src/firestore/types.ts
+++ src/firestore/types.ts
@@ -1,3 +1,19 @@
 import * as firestoreClient from "@firebase/firestore-types";
 import * as firestoreAdmin from "@google-cloud/firestore";
 export {firestoreClient, firestoreAdmin};
+
+declare module "@firebase/firestore-types" {
+    export interface FirebaseFirestore {
+        collection<T = DocumentData>(collectionPath: string): CollectionReference<T>;
+        doc<T = DocumentData>(documentPath: string): DocumentReference<T>;
+    }
+}
+
+declare module "@google-cloud/firestore" {
+    export namespace FirebaseFirestore {
+        export interface Firestore {
+            collection<T = DocumentData>(collectionPath: string): CollectionReference<T>;
+            doc<T = DocumentData>(documentPath: string): DocumentReference<T>;
+        }
+    }
+}
