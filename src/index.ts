5b17822d454726dbb7dfb2bb7f32bed99d8dbff0
levy <levy@codeandmore.pl>
2020-12-03 16:10:20 +0100

    Commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {FirebaseContext, FirebaseContextFirestore} from "./context";
+export {FirebaseContext, FirebaseContextFirestore} from "./FirebaseContext";
 export {isFirebaseAdmin, isFirebaseClient, setFirebaseMode} from "./mode";
 export {FirebaseAdminModule} from "./FirebaseAdminModule";
 export {FirebaseClientModule} from "./FirebaseClientModule";
